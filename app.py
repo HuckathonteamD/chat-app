8105d119dc5901a3f1db6426fea7b87d392c0ab3
hayashi-mf <114895305+hayashi-mf@users.noreply.github.com>
2022-11-03 13:36:38 +0900

    メッセージ編集機能追加、メッセージ表示方法をJSに変更

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, redirect, render_template, session, flash
 from models import dbConnect
 from util.user import User
+from util.dateformat import dateFormat
 from datetime import timedelta, datetime, timezone
 import hashlib
 import uuid
@@ -132,7 +133,8 @@ def detail(cid):
         return redirect('/login')
     cid = cid
     channel = dbConnect.getChannelById(cid)
-    messages = dbConnect.getMessageAll(cid)
+    messages_old = dbConnect.getMessageAll(cid)
+    messages = dateFormat.getMessages(messages_old)
     follows = dbConnect.getFollowById(cid)
     return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
 
@@ -149,7 +151,8 @@ def update_channel():
 
     dbConnect.updateChannel(uid, channel_name, channel_description, current_date, cid)
     channel = dbConnect.getChannelById(cid)
-    messages = dbConnect.getMessageAll(cid)
+    messages_old = dbConnect.getMessageAll(cid)
+    messages = dateFormat.getMessages(messages_old)
     follows = dbConnect.getFollowById(cid)
     return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
 
@@ -167,7 +170,8 @@ def add_message():
         dbConnect.createMessage(uid, channel_id, message, current_date)
 
     channel = dbConnect.getChannelById(channel_id)
-    messages = dbConnect.getMessageAll(channel_id)
+    messages_old = dbConnect.getMessageAll(channel_id)
+    messages = dateFormat.getMessages(messages_old)
     follows = dbConnect.getFollowById(channel_id)
 
     return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
@@ -178,15 +182,36 @@ def delete_message():
     uid = session.get("uid")
     if uid is None:
         return redirect('/login')
-    message_id = request.form.get('message_id')
+    mid = request.form.get('message_id')
     cid = request.form.get('channel_id')
-    if message_id:
-        dbConnect.deleteMessage(message_id)
+    if mid:
+        dbConnect.deleteMessage(mid)
 
     channel = dbConnect.getChannelById(cid)
-    messages = dbConnect.getMessageAll(cid)
+    messages_old = dbConnect.getMessageAll(cid)
+    messages = dateFormat.getMessages(messages_old)
     follows = dbConnect.getFollowById(cid)
 
+    return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
+
+
+@app.route('/update_message', methods=['POST'])
+def update_message():
+    uid = session.get("uid")
+    if uid is None:
+        return redirect('/login')
+    cid = request.form.get('cid')
+    mid = request.form.get('mid')
+    message = request.form.get('update-message')
+    current_date = datetime.now(timezone(timedelta(hours=9)))
+
+    message_uid = dbConnect.getUserIdByMessageId(mid)
+    if message_uid["uid"] == uid:
+        dbConnect.updateMessage(uid, cid, message, current_date, mid)
+    channel = dbConnect.getChannelById(cid)
+    messages_old = dbConnect.getMessageAll(cid)
+    messages = dateFormat.getMessages(messages_old)
+    follows = dbConnect.getFollowById(cid)
     return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
 
 
@@ -204,7 +229,8 @@ def follow_channel(cid):
         
         dbConnect.followChannel(uid, cid)
         channel = dbConnect.getChannelById(cid)
-        messages = dbConnect.getMessageAll(cid)
+        messages_old = dbConnect.getMessageAll(cid)
+        messages = dateFormat.getMessages(messages_old)
         follows = dbConnect.getFollowById(cid)
 
         return render_template('detail.html', messages=messages, channel=channel, uid=uid, follows=follows)
